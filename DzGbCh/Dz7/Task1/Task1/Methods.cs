e1692cae45f81d20624e4cc71dba94b0b2821b53
dmitry ronzhin <d.ronzhin81@yandex.ru>
2023-01-25 22:05:53 +0500

    Complite task7

--- DzGbCh/Dz7/Task1/Task1/Methods.cs
+++ DzGbCh/Dz7/Task1/Task1/Methods.cs
@@ -21,7 +21,7 @@ namespace Task1
             {
                 for (int j = 0; j < column; j++)
                 {
-                    array[i,j] = Math.Round(Random.(min, max), valueRound);
+                    array[i,j] = Math.Round(min + random.NextDouble() * (max - min), valueRound);
                 }
             }
             return array;
@@ -37,6 +37,69 @@ namespace Task1
                     Console.Write(array[i,j] + "\t");
                 }
                 Console.WriteLine();
+            }
+        }
+        /// <summary>
+        /// Метод проверяет, есть ли число в массиве
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="findNumber">Число которое необходимо найти</param>
+        /// <returns>Выводит -1, если нет числа. если есть выводит число, которое мы ищем</returns>
+        public static double FindValueTwoDimensionalArray(double[,] array, double findNumber)
+        {
+            double result = -1;
+            int line = array.GetLength(0);
+            int column = array.GetLength(1);
+            for (int i = 0; i < line; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    if (array[i,j] == findNumber) result = findNumber;
+                }
+            }
+            return result;
+        }
+        /// <summary>
+        ///  Метод выводит строку со средними аривметическими значениями.
+        ///  Если column равен 1, то по колонкам.
+        ///  Если row равен 1, то по колонкам.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="column"></param>
+        /// <param name="row"></param>
+        public static void AverageRowColumn2Array(double[,] array, int column = 0, int row = 0) 
+        {
+            int rows = array.GetLength(0);
+            int columns = array.GetLength(1);
+            double result = 0;
+            if (row == 1)
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        result += array[i, j];
+                    }
+                    result = Math.Round(result / rows, 1);
+                    Console.Write(result + "\t");
+                    result= 0;
+                }
+            }
+            else if(column == 1) 
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        result += array[i, j];
+                    }
+                    result = Math.Round(result / columns, 1);
+                    Console.Write(result + "\t");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Не выбрано, что искать");
             }
         }
     }
--- DzGbCh/Dz7/Task1/Task1/Program.cs
+++ DzGbCh/Dz7/Task1/Task1/Program.cs
@@ -4,34 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//Задача 47.Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-//m = 3, n = 4.
-//0,5 7 -2 -0,2
-//1 -3,3 8 -9,9
-//8 7,8 -7,1 9
-//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
-//Например, задан массив:
-//1 4 7 2
-//5 9 2 3
-//8 4 2 4
-//17->такого числа в массиве нет
-//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-//Например, задан массив:
-//1 4 7 2
-//5 9 2 3
-//8 4 2 4
-//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
-
 namespace Task1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //Задача 47.Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+            //m = 3, n = 4.
+            //0,5 7 -2 -0,2
+            //1 -3,3 8 -9,9
+            //8 7,8 -7,1 9
+
             int line = Methods.GetValue("Введите количество строк массива");
             int column = Methods.GetValue("Введите количество столбцов массива");
-            double[,] userArray = Methods.CreateRandomTwoDimensionalArray(line, column);
-            Methods.PrintTwoDimensionalArray(userArray);
+            double[,] userArray1 = Methods.CreateRandomTwoDimensionalArray(line, column, max: 10, min: -10);
+            Methods.PrintTwoDimensionalArray(userArray1);
+
+            //Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+            //Например, задан массив:
+            //1 4 7 2
+            //5 9 2 3
+            //8 4 2 4
+            //17->такого числа в массиве нет
+
+            Console.WriteLine();
+            double[,] userArray2 = Methods.CreateRandomTwoDimensionalArray(line, column, max: 10, min: 1, valueRound: 0);
+            Methods.PrintTwoDimensionalArray(userArray2);
+            double findNumber = Methods.GetValue("Введите число, которое хотите найти в массиве");
+            double result = Methods.FindValueTwoDimensionalArray(userArray2, findNumber);
+            Console.WriteLine();
+            if (result == -1)
+            {
+                Console.WriteLine($"Число {findNumber} нет в заданном массиве");
+            }
+            else
+            {
+                Console.WriteLine($"Число {findNumber} есть в заданном массиве");
+            }
+
+            //Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+            //Например, задан массив:
+            //1 4 7 2
+            //5 9 2 3
+            //8 4 2 4
+            //Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+            Console.WriteLine();
+            Methods.AverageRowColumn2Array(userArray2, column: 1);
 
         }
     }
