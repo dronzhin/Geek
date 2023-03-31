b91b04f5c1ca48bac41e1915b95f4c65a8b58a3e
Dmytry Ronzhin <mrakrus25@mail.ru>
2023-03-31 16:27:20 +0500

    add dz2(java) task 2

--- JavaGB/Seminar/src/DZ2/Metods/MyFunction.java
+++ JavaGB/Seminar/src/DZ2/Metods/MyFunction.java
@@ -1,5 +1,8 @@
 package DZ2.Metods;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -22,5 +25,11 @@ public class MyFunction {
             array[i] = rand.nextInt(max * 2) - min;
         }
         return array;
+    }
+    public static String readUsingScanner(String fileName) throws IOException {
+        Scanner scanner = new Scanner(Paths.get(fileName), StandardCharsets.UTF_8.name());
+        String data = scanner.useDelimiter("\\A").next();
+        scanner.close();
+        return data;
     }
 }
--- JavaGB/Seminar/src/DZ2/Task2.java
+++ JavaGB/Seminar/src/DZ2/Task2.java
@@ -1,4 +1,25 @@
 package DZ2;
 
+import DZ2.Metods.MyFunction;
+
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Task2 {
+    public static void main(String[] args) throws IOException {
+        String text = MyFunction.readUsingScanner("src/DZ2/Metods/Text.json");
+        System.out.println(text);
+        String[] array = text.split("}");
+        String[] newArray = new String[array.length - 1];
+        for (int i = 0; i < newArray.length; i++) {
+            newArray[i] = array[i].replaceAll("[\\]\\[\\}\\{\"]", "");
+            newArray[i] = newArray[i].replaceAll("[:,]", " ");
+        }
+        for (int i = 0; i < newArray.length; i++) {
+            String[] arrayWord = newArray[i].trim().split(" ");
+            System.out.println("Студент " + arrayWord[1] + " получил " + arrayWord[3] + " по предмету " + arrayWord[5]);
+        }
+
+
+    }
 }
